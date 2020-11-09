e301337ad75ff9e21fe67a5b515465961baca4dd
philipp <popovph9@gmil.com>
2020-11-09 17:24:17 +0300

    Выполнено задание 19 учебного проекта: истина где-то на сервере

--- src/store/action.js
+++ src/store/action.js
@@ -1,4 +1,3 @@
-import {STEP} from "../const";
 import {isArtistAnswerCorrect, isGenreAnswerCorrect} from "../game";
 import {GameType} from "../const";
 
@@ -6,31 +5,35 @@ export const ActionType = {
   INCREMENT_MISTAKES: `INCREMENT_MISTAKES`,
   INCREMENT_STEP: `INCREMENT_STEP`,
   RESET_GAME: `RESET_GAME`,
+  LOAD_QUESTIONS: `LOAD_QUESTIONS`,
 };
 
-export const ActionCreator = {
-  incrementStep: () => ({
-    type: ActionType.INCREMENT_STEP,
-    payload: STEP,
-  }),
-  resetGame: () => ({
-    type: ActionType.RESET_GAME,
-  }),
-  incrementMistake: (question, userAnswer) => {
-    let answerIsCorrect = false;
+export const incrementStep = () => ({
+  type: ActionType.INCREMENT_STEP,
+  payload: 1,
+});
 
-    switch (question.type) {
-      case GameType.ARTIST:
-        answerIsCorrect = isArtistAnswerCorrect(question, userAnswer);
-        break;
-      case GameType.GENRE:
-        answerIsCorrect = isGenreAnswerCorrect(question, userAnswer);
-        break;
-    }
+export const resetGame = () => ({
+  type: ActionType.RESET_GAME,
+});
 
-    return {
-      type: ActionType.INCREMENT_MISTAKES,
-      payload: answerIsCorrect ? 0 : 1,
-    };
-  },
+export const incrementMistake = (question, userAnswer) => {
+  let answerIsCorrect = false;
+  switch (question.type) {
+    case GameType.ARTIST:
+      answerIsCorrect = isArtistAnswerCorrect(question, userAnswer);
+      break;
+    case GameType.GENRE:
+      answerIsCorrect = isGenreAnswerCorrect(question, userAnswer);
+      break;
+  }
+  return {
+    type: ActionType.INCREMENT_MISTAKES,
+    payload: answerIsCorrect ? 0 : 1,
+  };
 };
+
+export const loadQuestions = (questions) => ({
+  type: ActionType.LOAD_QUESTIONS,
+  payload: questions,
+});

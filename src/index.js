b9b77d3c7d7ac825327aabe892c1e74886ec718e
Popovph9 <58945032+Popovph9@users.noreply.github.com>
2020-11-10 22:45:10 +0300

    Выполнено задание 19 учебного проекта: истина где-то на сервере (#8)
    Co-authored-by: philipp <popovph9@gmil.com>

--- src/index.js
+++ src/index.js
@@ -1,19 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {createStore} from "redux";
+import {createStore, applyMiddleware} from "redux";
+import thunk from "redux-thunk";
+import {composeWithDevTools} from "redux-devtools-extension";
+import {createAPI} from "./services/api";
+import {fetchQuestionList} from "./store/api-actions";
 import {Provider} from "react-redux";
 import App from "./components/app/app";
 
-import {reducer} from "./store/reducer";
+import rootReducer from "./store/reducers/root-reducer";
+
+const api = createAPI();
 
 const store = createStore(
-    reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+    rootReducer,
+    composeWithDevTools(
+        applyMiddleware(thunk.withExtraArgument(api))
+    )
 );
 
-ReactDOM.render(
-    <Provider store={store}>
-      <App/>
-    </Provider>,
-    document.querySelector(`#root`)
-);
+Promise.all([
+  store.dispatch(fetchQuestionList()),
+])
+.then(() => {
+  ReactDOM.render(
+      <Provider store={store}>
+        <App />
+      </Provider>,
+      document.querySelector(`#root`)
+  );
+});
